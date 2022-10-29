80921ac9f8ce7bbe58d564faff0c3c73ef761f98
ecurtis28 <evancurtis28@protonmail.com>
2022-10-28 18:03:42 -0700

    Create basic image and photo carousel

---
+++ app.js
@@ -0,0 +1,14 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs")
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.render("index")
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
